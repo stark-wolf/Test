06b69b974f2f4bbabd1b0e35f6601cecbe2b4a97
Me <me@Mes-MacBook-Pro.local>
2015-12-27 16:47:55 +0300

    Test

--- Decide/ViewController.swift
+++ Decide/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Blue: UIImageView!
+    @IBOutlet weak var Red: UIImageView!
+    @IBOutlet weak var HideRed: UIButton!
+    @IBOutlet weak var HideBlue: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-
+   
 }
-
+    @IBAction func HideRed(sender: AnyObject) {
+        Blue.hidden = true
+        Red.hidden = false
+        HideBlue.hidden = false
+        HideRed.hidden = false
+    }
+    @IBAction func HideBlue(sender: AnyObject) {
+        Red.hidden = false
+        Blue.hidden = true
+        HideRed.hidden = false
+        HideBlue.hidden = false
+    }
+}
